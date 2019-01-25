e077aa389ff7b8bd96a84759e714d87c7721a706
as14g18 <as14g18@soton.ac.uk>
2019-01-25 22:52:49 +0000

    Fixed bug with collision

--- HomeHavoc/scenes/BottomBrick.gd
+++ HomeHavoc/scenes/BottomBrick.gd
@@ -14,10 +14,10 @@ func _integrate_forces(state):
 		connected_to_drone = true
 		set_use_custom_integrator(true) 
 		body_on_which_sticked = state.get_contact_collider_object(0)
-		print("The ball is sticking on a ", body_on_which_sticked.get_name())
-		var tr_ci_world_to_ball = get_global_transform()
+		print("The block is sticking on a ", body_on_which_sticked.get_name())
+		var tr_ci_world_to_block = get_global_transform()
 		var tr_ci_world_to_collider = body_on_which_sticked.get_global_transform()
-		tr_ci_collider_to_ball = tr_ci_world_to_collider.inverse() * tr_ci_world_to_ball
+		tr_ci_collider_to_block = tr_ci_world_to_collider.inverse() * tr_ci_world_to_block
 
 func _on_Area2D_area_entered(area):
 	connected_to_drone = true
