aa17b699aa6b2452aa584ef54e4d70e36a87afa8
as14g18 <as14g18@soton.ac.uk>
2019-01-26 12:33:46 +0000

    Added new block type large block

--- HomeHavoc/scenes/BottomBrick.gd
+++ HomeHavoc/scenes/BottomBrick.gd
@@ -1,8 +1,10 @@
 extends RigidBody2D
 
 var connected_to_drone
+const BLOCK_TYPES = ["SmallBlock", "LargeBlock"]
 
 func _ready():
+	block_type = BLOCK_TYPES[0] # random number
 	position = Vector2(rand_range(get_viewport().size.x / 3, get_viewport().size.x * 2 / 3), 10)
 
 func _physics_process(delta):
