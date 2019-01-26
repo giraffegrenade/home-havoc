069f27da2550f35a211f9c4aa7a77d38586d35bc
as14g18 <as14g18@soton.ac.uk>
2019-01-26 00:30:33 +0000

    Added fall delay

--- HomeHavoc/scenes/Main.gd
+++ HomeHavoc/scenes/Main.gd
@@ -2,6 +2,6 @@ extends Node
 
 const BOTTOM_BRICK_SCENE = preload("res://scenes/BottomBrick.tscn")
 
-func _process(delta):
+func _on_SpawnTimer_timeout():
 	var clone = BOTTOM_BRICK_SCENE.instance()
 	add_child(clone)
