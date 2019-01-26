a4febfdafd00362828d1e0b46c87427cf77ef57d
as14g18 <as14g18@soton.ac.uk>
2019-01-26 00:22:23 +0000

    Blocks now spawn at top of screen

--- HomeHavoc/scenes/BottomBrick.gd
+++ HomeHavoc/scenes/BottomBrick.gd
@@ -2,6 +2,9 @@ extends RigidBody2D
 
 var connected_to_drone
 
+func _ready():
+	position = Vector2(rand_range(get_viewport().size.x / 3, get_viewport().size.x * 2 / 3), 10)
+
 func _physics_process(delta):
 	if connected_to_drone:
 		$CollisionShape2D.disabled = true
--- HomeHavoc/scenes/Main.gd
+++ HomeHavoc/scenes/Main.gd
@@ -1,2 +1,7 @@
 extends Node
 
+const BOTTOM_BRICK_SCENE = preload("res://scenes/BottomBrick.tscn")
+
+func _process(delta):
+	var clone = BOTTOM_BRICK_SCENE.instance()
+	add_child(clone)
