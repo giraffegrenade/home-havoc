0a64c398fbafe57707701eb6e2ed9b4fb0e6d772
as14g18 <as14g18@soton.ac.uk>
2019-01-26 17:27:47 +0000

    Fixed bug with confetti

--- HomeHavoc/scenes/Main.gd
+++ HomeHavoc/scenes/Main.gd
@@ -69,17 +69,16 @@ func _on_GameTimer_timeout():
 		show_confetti()
 	else:
 		$Arrow/GameEndLabel.text = "Draw!"
+	
+	$ExitGameButton.show()
+	$RestartButton.show()
 
 func show_confetti():
 	$ConfettiHolder/Confetti.show()
 	$ConfettiHolder/Confetti2.show()
-		
-	$ExitGameButton.show()
-	$RestartButton.show()
 
 func _on_RestartButton_pressed():
 	get_tree().change_scene("res://scenes/Main.tscn")
-
 
 func _on_ExitGameButton_pressed():
 	get_tree().change_scene("res://scenes/MainMenu.tscn")
