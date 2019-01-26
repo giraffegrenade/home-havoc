807078c712a410b57bd938ff60985e782cedf1df
as14g18 <as14g18@soton.ac.uk>
2019-01-26 14:27:32 +0000

    Implemented check for tallest tower

--- HomeHavoc/scenes/BottomBrick.gd
+++ HomeHavoc/scenes/BottomBrick.gd
@@ -3,6 +3,8 @@ extends RigidBody2D
 var connected_to_drone
 
 func _ready():
+	set_contact_monitor(true)
+	set_max_contacts_reported(16)
 	position = Vector2(rand_range(get_viewport().size.x / 3, get_viewport().size.x * 2 / 3), 10)
 
 func _physics_process(delta):
