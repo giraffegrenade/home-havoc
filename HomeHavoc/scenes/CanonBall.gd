b46d83c461dc1f0a65f2d2ae6dd4806fab3bb05b
Jack Parsons <jack.parsons.uk@gmail.com>
2019-01-27 09:00:33 +0000

    Fixing cannon firing direction

--- HomeHavoc/scenes/CanonBall.gd
+++ HomeHavoc/scenes/CanonBall.gd
@@ -3,4 +3,4 @@ extends RigidBody2D
 const LAUNCH_SPEED = 1000
 
 func launch(direction):
-	apply_impulse(position - direction, (position - direction).normalized() * LAUNCH_SPEED * -1)
+	apply_impulse(direction * 10, (direction).normalized() * LAUNCH_SPEED * -1)
