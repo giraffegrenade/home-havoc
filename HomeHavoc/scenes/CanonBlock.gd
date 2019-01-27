46cb6beb3f7e7eebe0b54c2d475c8cf1816ec3b0
as14g18 <as14g18@soton.ac.uk>
2019-01-27 09:02:44 +0000

    Limited cannon to only fire when placed

--- HomeHavoc/scenes/CanonBlock.gd
+++ HomeHavoc/scenes/CanonBlock.gd
@@ -3,7 +3,8 @@ extends "res://scenes/BottomBrick.gd"
 const CANON_BALL_SCENE = preload("res://scenes/CanonBall.tscn")
 
 func _on_CanonTimer_timeout():
-	var ball = CANON_BALL_SCENE.instance()
-	ball.position = position - Vector2(cos(rotation), sin(rotation)) * 12
-	get_parent().add_child(ball)
-	ball.launch(Vector2(cos(rotation), sin(rotation)))
+	if !connected_to_drone:
+		var ball = CANON_BALL_SCENE.instance()
+		ball.position = position - Vector2(cos(rotation), sin(rotation)) * 12
+		get_parent().add_child(ball)
+		ball.launch(Vector2(cos(rotation), sin(rotation)))
