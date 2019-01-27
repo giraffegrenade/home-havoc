009324f26eda9984b5c7cb1f54dfaed4cae32680
as14g18 <as14g18@soton.ac.uk>
2019-01-27 07:26:13 +0000

    Canon changes

--- HomeHavoc/scenes/CanonBlock.gd
+++ HomeHavoc/scenes/CanonBlock.gd
@@ -1,4 +1,9 @@
 extends "res://scenes/BottomBrick.gd"
 
+const CANON_BALL_SCENE = preload("res://scenes/CanonBrick.tscn")
+
 func _on_CanonTimer_timeout():
-	pass
+	var ball = CANON_BALL_SCENE.scene.instance()
+	ball.position = position
+	add_child(ball)
+	ball.launch(Vector2(position.x - 10, position.y))
